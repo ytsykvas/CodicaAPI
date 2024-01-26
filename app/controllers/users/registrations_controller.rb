420102ca6621457cffd91f8d530adfcce69ac625
Yurii Tsykvas <tsykvasyurii@gmail.com>
2024-01-26 19:05:00 +0200

    Refactored registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,25 +3,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
-    build_resource(sign_up_params)
-
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
-      if resource.active_for_authentication?
-        sign_up(resource_name, resource)
-
-        # for getting user`s authentication_token in postman for reviewer
-        render json: resource
+    super do |user|
+      if user.persisted?
+        render json: user
       else
-        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        clean_up_passwords user
+        set_minimum_password_length
+        respond_with user
       end
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
+      return
     end
   end
 end
